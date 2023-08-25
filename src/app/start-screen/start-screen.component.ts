e2f9864731be1430774b47c853d388d45ee05496
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-08-25 18:47:02 +0200

    create new game

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import {
+  CollectionReference,
+  DocumentData,
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+  doc,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,9 +24,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./start-screen.component.scss'],
 })
 export class StartScreenComponent {
-  constructor(private router: Router) {}
+  private gameCollection: CollectionReference<DocumentData>;
+  public game: any
 
-  newGame() {
-    this.router.navigateByUrl('/game')
+  constructor(private readonly firestore: Firestore, private router: Router) {
+    this.gameCollection = collection(this.firestore, 'game');
+    console.log(this.gameCollection);
+  }
+  
+  async newGame() {
+    this.game = new Game();
+    this.game = this.game.toJson();
+    const gameInfo = await addDoc(this.gameCollection, this.game);
+    console.log(gameInfo['id']);
+    this.router.navigateByUrl('/game/' + gameInfo['id'])
   }
 }
